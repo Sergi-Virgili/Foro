a8759fec2c877f78c80b5ed6cc01a5e296842d39
Sergi Virgili <svirgilif@gmail.com>
2019-09-26 15:46:03 +0200

    PRE ENTRAGA CSS ENVELLECEDOR

--- resources/views/foro/area.blade.php
+++ resources/views/foro/area.blade.php
@@ -11,16 +11,18 @@
         @foreach ($areas as $area)
         <li><a href="/foro/{{$area->id}}/temas">{{$area->name}}</a></li>
         <li>{{$area->description}}</li>
+        <div>
         <form method="GET" action="/foro/area/{{$area->id}}/edit">
             @csrf
             @method('get')
-            <input type="submit" value="Edit">
+            <input type="submit" class="btn btn-outline-primary btn-sm " value="Edit">
         </form>
         <form method="POST" action="/foro/{{$area->id}}">
             @csrf
             @method('delete')
-            <input type="submit" value="Delete">
+            <input type="submit" class="btn btn-outline-danger btn-sm " value="Delete">
         </form>
+        </div>
         @endforeach
     </ul>
     <a role="btn" class="btn" href="/foro/create" value="new area">Crear</a>
--- resources/views/foro/updateTheme.blade.php
+++ resources/views/foro/updateTheme.blade.php
@@ -1,7 +1,7 @@
 @extends('../layouts.app')
 
 @section('content')
-
+{{$theme->area->name}}
 <h1>Edit Hilo</h1>
 <form action="/foro/tema/{{$theme->id}}" method="post">
       
@@ -15,12 +15,12 @@
         <div class="form-group">
                 <label for="area">Selecciona Area</label>
         {{-- TODO: area seleccionada --}}
-        <select class="form-control" id="area_id" name="area_id" value="{{$theme->area}}">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
+        <select class="form-control" id="area_id" name="area_id" value="{{$theme->area->id}}">
+                {{-- @foreach ($theme->area as $area)
+                        <option>{{$area->name}}</option>
+                @endforeach --}}
+                       
+                  
                 </select>
         </div>
         <div class="form-group">
